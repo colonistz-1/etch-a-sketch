71ad46a77ec5c481e1ce97d34aeea7ac1e31a085
colonistz-1 <tony.nguyen2415@gmail.com>
2023-04-15 12:35:51 -0700

    Add 16x16 Grid

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const grid = document.querySelector("#grid")
+
+function makeDivs(num) {
+    for (let i = 0; i < num; i++){
+        let cells = document.createElement('div');
+        cells.classList.add("square");
+        grid.appendChild(cells);    
+        }
+}
+    
+makeDivs(256);
