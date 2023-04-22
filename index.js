b312d869800d5be5f36f26f50bfee7be3c9e81e2
colonistz-1 <tony.nguyen2415@gmail.com>
2023-04-21 19:29:10 -0700

    added functional canvas board

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const grid = document.querySelector("#grid")
+const eraser = document.querySelector("#erase")
+ctx = grid.getContext("2d");
 
 function makeDivs(num) {
     for (let i = 0; i < num; i++){
         let cells = document.createElement('div');
         cells.classList.add("square");
+        cells.style.columnGap = "40px"
         grid.appendChild(cells);    
         }
 }
-    
 makeDivs(256);
+
+const drawing = (e) =>{
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+}
+
+grid.addEventListener("mousemove", drawing);
+
+eraser.addEventListener("click", ()=>{
+    ctx.clearRect(0,0, grid.width, grid.height);
+})
+
