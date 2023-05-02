aa6ad205f448587e95c2773b37af1d57f82f32b6
colonistz-1 <tony.nguyen2415@gmail.com>
2023-05-01 21:22:21 -0700

    implemented colors

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const grid = document.querySelector("#grid")
 const eraser = document.querySelector("#erase")
+const redbtn = document.querySelector("#red")
+const bluebtn = document.querySelector("#blue")
+const greenbtn = document.querySelector("#green")
+const orangebtn = document.querySelector("#orange")
+const purplebtn = document.querySelector("#purple")
+const blackbtn = document.querySelector("#black")
 ctx = grid.getContext("2d");
 const brushslider = document.querySelector("#brush-size");
 let brushThickness = brushslider.value; 
@@ -39,8 +45,10 @@ const drawing = (e) =>{
 grid.addEventListener("mousedown", startDraw)
 grid.addEventListener("mousemove", drawing)
 grid.addEventListener("mouseup", () => isDrawing =false)
-eraser.addEventListener("click", () => location.reload()
-)
-
-
-
+eraser.addEventListener("click", () => location.reload())
+redbtn.addEventListener("click", () => ctx.strokeStyle = 'rgb(248, 14, 14)');
+bluebtn.addEventListener("click", () => ctx.strokeStyle = 'rgba(6, 77, 230, 0.98)');
+greenbtn.addEventListener("click", () => ctx.strokeStyle = 'rgba(7, 223, 17, 0.98)');
+orangebtn.addEventListener('click', () => ctx.strokeStyle = 'rgb(247, 121, 4)');
+purplebtn.addEventListener("click", () => ctx.strokeStyle = "rgb(140, 6, 250)");
+blackbtn.addEventListener('click', () => ctx.strokeStyle = "black");
